89c72433357d0e6989fff4fad1517645275251ca
tchiunam <tchiunam@users.noreply.github.com>
2022-09-05 21:21:31 +0800

    Load test redis cluster with random keys

--- bin/redis-cluster-load-test.py
+++ bin/redis-cluster-load-test.py
@@ -7,6 +7,41 @@ from locust import User, events, tag, task
 from rediscluster import RedisCluster
 
 
+def random_LDP(length: int = 10) -> str:
+    """
+    Generate a simple random string with a length of 10 characters with
+    letters, digits and punctuation.
+
+    :param length: Length of the string. Default is 10.
+    :type length: int
+
+    :return: Random string.
+    :rtype: str
+    """
+
+    return generate_random_string(
+        length=length,
+        with_digits=True,
+        with_punctuation=True
+    )
+
+
+def get_response_time_in_ms(start_time: float, end_time: float) -> int:
+    """
+    Get the response time in milliseconds.
+
+    :param start_time: Start time.
+    :type start_time: float
+    :param end_time: End time.
+    :type end_time: float
+
+    :return: Response time in milliseconds.
+    :rtype: int
+    """
+
+    return int((end_time - start_time) * 1000)
+
+
 class RedisClient(object):
     """
     A redis client to perform load test on redis cluster.
@@ -45,30 +80,32 @@ class RedisClient(object):
 
         request_type = "SET"
         result: str = None
+        value = random_LDP()
 
         start_time = time.time()
         try:
-            result = self.rc.set(
-                name=name,
-                value=generate_random_string(
-                    length=10,
-                    with_digits=True,
-                    with_punctuation=True))
+            result = self.rc.set(name=name, value=value)
         except Exception as e:
-            total_time = int((time.time() - start_time) * 1000)
             events.request_failure.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                exception=e)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                exception=e
+            )
         else:
-            total_time = int((time.time() - start_time) * 1000)
             length = len(str(result))
             events.request_success.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                response_length=length)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                response_length=length
+            )
 
         return result
 
@@ -90,20 +127,26 @@ class RedisClient(object):
         try:
             result = self.rc.get(name=name)
         except Exception as e:
-            total_time = int((time.time() - start_time) * 1000)
             events.request_failure.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                exception=e)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                exception=e
+            )
         else:
-            total_time = int((time.time() - start_time) * 1000)
             length = len(str(result))
             events.request_success.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                response_length=length)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                response_length=length
+            )
 
         return result
 
@@ -120,32 +163,35 @@ class RedisClient(object):
 
         request_type = "LPUSH"
         result: int = -1
-        size = random.randint(1, 5)
-        elements = list()
-        for _ in range(size):
-            elements.append(generate_random_string(
-                length=random.randint(5, 10),
-                with_digits=True,
-                with_punctuation=True))
+        # Create a list of random length with random elements.
+        elements = [random_LDP(
+            length=random.randint(5, 10)
+        )] * random.randint(1, 5)
 
         start_time = time.time()
         try:
             result = self.rc.lpush(name, *elements)
         except Exception as e:
-            total_time = int((time.time() - start_time) * 1000)
             events.request_failure.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                exception=e)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                exception=e
+            )
         else:
-            total_time = int((time.time() - start_time) * 1000)
             length = result.bit_length()
             events.request_success.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                response_length=length)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                response_length=length
+            )
 
         return result
 
@@ -162,32 +208,38 @@ class RedisClient(object):
 
         request_type = "SADD"
         result: int = -1
+        # Create a set of random length with random members.
         size = random.randint(1, 5)
         members = set()
         for _ in range(size):
-            members.add(generate_random_string(
+            members.add(random_LDP(
                 length=random.randint(5, 10),
-                with_digits=True,
-                with_punctuation=True))
+            ))
 
         start_time = time.time()
         try:
             result = self.rc.sadd(name, *members)
         except Exception as e:
-            total_time = int((time.time() - start_time) * 1000)
             events.request_failure.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                exception=e)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                exception=e
+            )
         else:
-            total_time = int((time.time() - start_time) * 1000)
             length = len(str(result))
             events.request_success.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                response_length=length)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                response_length=length
+            )
 
         return result
 
@@ -207,30 +259,32 @@ class RedisClient(object):
         elements = list()
         for i in range(4):
             elements.append(str(i))
-            elements.append(
-                generate_random_string(
-                    length=random.randint(5, 10),
-                    with_digits=True,
-                    with_punctuation=True))
+            elements.append(random_LDP(length=random.randint(5, 10)))
 
         start_time = time.time()
         try:
             result = self.rc.hset(name, *elements)
         except Exception as e:
-            total_time = int((time.time() - start_time) * 1000)
             events.request_failure.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                exception=e)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                exception=e
+            )
         else:
-            total_time = int((time.time() - start_time) * 1000)
             length = len(str(result))
             events.request_success.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                response_length=length)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                response_length=length
+            )
 
         return result
 
@@ -250,24 +304,28 @@ class RedisClient(object):
 
         start_time = time.time()
         try:
-            result = self.rc.hget(
-                name=name,
-                key=random.randint(0, 3))
+            result = self.rc.hget(name=name, key=random.randint(0, 3))
         except Exception as e:
-            total_time = int((time.time() - start_time) * 1000)
             events.request_failure.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                exception=e)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                exception=e
+            )
         else:
-            total_time = int((time.time() - start_time) * 1000)
             length = len(str(result))
             events.request_success.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                response_length=length)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                response_length=length
+            )
 
         return result
 
@@ -289,19 +347,26 @@ class RedisClient(object):
         try:
             result = self.rc.hdel(name, random.randint(0, 3))
         except Exception as e:
-            total_time = int((time.time() - start_time) * 1000)
             events.request_failure.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time, exception=e)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                exception=e
+            )
         else:
-            total_time = int((time.time() - start_time) * 1000)
             length = len(str(result))
             events.request_success.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                response_length=length)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                response_length=length
+            )
 
         return result
 
@@ -318,39 +383,50 @@ class RedisClient(object):
 
         request_type = "ZADD"
         result: int = -1
-        member = generate_random_string(
-            length=5,
-            with_digits=True,
-            with_punctuation=True)
+        member = random_LDP(length=5)
         score = random.randint(1, 100)
 
         start_time = time.time()
         try:
-            result = self.rc.zadd(name, {member: score}, nx=False)
+            result = self.rc.zadd(name=name, mapping={member: score}, nx=False)
         except Exception as e:
-            total_time = int((time.time() - start_time) * 1000)
             events.request_failure.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                exception=e)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                exception=e
+            )
         else:
-            total_time = int((time.time() - start_time) * 1000)
             length = len(result.encode())
             events.request_success.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                response_length=length)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                response_length=length
+            )
 
         return result
 
-    def get_sorted_set_members(self, name: str) -> list:
+    def get_sorted_set_range(
+            self,
+            name: str,
+            start: int = 0,
+            end: int = -1) -> list:
         """
-        Get members from a sorted set.
+        Get a range of members from a sorted set.
 
         :param name: The name of the sorted set.
         :type name: str
+        :param start: The start index of the sorted set. Default is 0.
+        :type start: int
+        :param end: The end index of the sorted set. Default is -1.
+        :type end: int
 
         :return: The members of the sorted set replied from redis server.
         :rtype: list
@@ -361,21 +437,28 @@ class RedisClient(object):
 
         start_time = time.time()
         try:
-            result = self.rc.zrange(name, 0, 100)
+            result = self.rc.zrange(name=name, start=start, end=end)
         except Exception as e:
-            total_time = int((time.time() - start_time) * 1000)
             events.request_failure.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time, exception=e)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                exception=e
+            )
         else:
-            total_time = int((time.time() - start_time) * 1000)
             length = len(str(result))
             events.request_success.fire(
                 request_type=request_type,
                 name=name,
-                response_time=total_time,
-                response_length=length)
+                response_time=get_response_time_in_ms(
+                    start_time=start_time,
+                    end_time=time.time()
+                ),
+                response_length=length
+            )
 
         return result
 
@@ -398,22 +481,67 @@ class RedisUserStaticKey(User):
     @ task
     @ tag("list")
     def list(self):
-        self._client.push_list_elements("list_lpush_operation")
+        self._client.push_list_elements(name="list_load_test")
 
     @ task
     @ tag("set")
     def set(self):
-        self._client.add_set_members("set_sadd_operation")
+        self._client.add_set_members(name="set_load_test")
 
     @ task
     @ tag("hash")
     def hash(self):
-        self._client.set_hash_elements("hash_load_test")
-        self._client.get_hash_element("hash_load_test")
-        self._client.del_hash_element("hash_load_test")
+        self._client.set_hash_elements(name="hash_load_test")
+        self._client.get_hash_element(name="hash_load_test")
+        self._client.del_hash_element(name="hash_load_test")
 
     @ task
     @ tag("sorted-set")
     def sorted_set(self):
-        self._client.add_sorted_set_member("sorted_set_add_load_test")
-        self._client.get_sorted_set_members("sorted_set_range_load_test")
+        self._client.add_sorted_set_member(name="sorted_set_add_load_test")
+        self._client.get_sorted_set_range(
+            name="sorted_set_range_load_test",
+            end=100
+        )
+
+
+class RedisUserRandomKey(User):
+    """
+    A user that uses random keys.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._client = RedisClient()
+
+    @ task
+    @ tag("string")
+    def string(self):
+        name = random_LDP()
+        self._client.set_string(name=name)
+        self._client.get_string(name=name)
+
+    @ task
+    @ tag("list")
+    def list(self):
+        self._client.push_list_elements(name=random_LDP())
+
+    @ task
+    @ tag("set")
+    def set(self):
+        self._client.add_set_members(name=random_LDP())
+
+    @ task
+    @ tag("hash")
+    def hash(self):
+        name = random_LDP()
+        self._client.set_hash_elements(name=name)
+        self._client.get_hash_element(name=name)
+        self._client.del_hash_element(name=name)
+
+    @ task
+    @ tag("sorted-set")
+    def sorted_set(self):
+        name = random_LDP()
+        self._client.add_sorted_set_member(name=name)
+        self._client.get_sorted_set_range(name=name)
